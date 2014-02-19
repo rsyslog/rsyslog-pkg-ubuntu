e908ab9ad13e5040baf4de7f06ff2b2950677c0c
Andre Lorbach <alorbach@adiscon.com>
2014-02-19 10:07:49 +0100

    Added ppa upload script

---
+++ scripts/uploadppa.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Definitions common to these scripts
+source $(dirname "$0")/config.sh
+
+# If we assume the directory is named after the package,
+PACKAGENAME=$(basename `readlink -f .`)
+TARGZFILES=` ls -d */ | grep $PACKAGENAME`
+# TARGZFILES=` ls -d */`
+
+echo "--------------------------------------"
+echo "--- Prep Release and Upload to PPA ---"
+echo "--------------------------------------"
+select szPrepareDir in $TARGZFILES
+do
+	echo "Select BRANCH:"
+	select szBranch in $BRANCHES
+        do
+        	echo "Select Ubuntu DIST:"
+        	select szPlatform in $PLATFORM
+        	do
+        	        break;
+        	done
+        	break;
+	done
+	break;
+done
+
+#szSourceBase=`basename $szSourceFile .tar.gz`
+#szSourceBase=`echo $szSourceBase | sed 's/_/-/'`
+#
+#echo "$szSourceBase";
+#exit; 
+
+#tar xfz $szSourceFile 
+#mv $szSourceFile $szReplaceFile.orig.tar.gz
+
+read -p "Generate Changelog entry for $szPlatform/$szBranch/$szDebian (y/n)? " RESULT
+if [ "$RESULT" == "y" ]; then
+	cp -r $szPlatform/$szBranch/debian $szPrepareDir
+	cd $szPrepareDir
+	dch -D $szPlatform -i
+fi
+
+# Build Source package now!
+debuild -S -sa -rfakeroot -k"$KEY_ID"
+
+if [ "$RESULT" == "y" ]; then
+	# Save Changes back now
+	cd ..
+	szDebian="debian"
+	echo    # (optional) move to a new line
+	read -p "Copy $szPrepareDir$szDebian folder back to $szPlatform/$szBranch/$szDebian (y/n)? " RESULT
+	echo    # (optional) move to a new line 
+	if [ "$RESULT" == "y" ]; then
+		cp -r $szPrepareDir$szDebian $szPlatform/$szBranch/
+		echo "$szPrepareDir$szDebian copied back."
+	fi
+fi
+
+CHANGESFILES=`ls *.changes`
+
+echo "-------------------------------------"
+echo "--- Select change file for upload ---"
+echo "-------------------------------------"
+
+echo "Select Changefile:"
+select szChangeFile in $CHANGESFILES
+do
+        break;
+done
+
+# Upload changes to PPA now!
+dput -f ppa:adiscon/$szBranch $szChangeFile
+
