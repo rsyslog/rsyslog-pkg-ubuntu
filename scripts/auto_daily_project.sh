f032398ca19b773be1ac8f86fc4e451aec2e2098
Rainer Gerhards <rgerhards@adiscon.com>
2015-01-21 16:10:08 +0100

    need to generate different version names for daily launchpad build

--- scripts/auto_daily_project.sh
+++ scripts/auto_daily_project.sh
@@ -2,6 +2,13 @@
 # Build daily snapshot rsyslog package
 # ***current working directory must be project dir (e.g. rsyslog)***
 
+# Note: Launchpad does not work with the git hash inside the version
+# number, because it checks if the version number as whole is larger
+# than what exists. This obviously is not always the case with hashes.
+# As such, we rename the version to todays date and time. It looks like
+# this works sufficiently good, even when the rsyslog code has the
+# "right" (hash-based) version number.
+
 #set -o xtrace
 
 # params
@@ -12,7 +19,8 @@ szBranch=$2
 szSourceFile=`ls *.tar.gz`
 szSourceBase=`basename $szSourceFile .tar.gz`
 VERSION=`echo $szSourceBase|cut -d- -f2`
-szReplaceFile=`echo $szSourceBase | sed 's/-/_/'`
+LAUNCHPAD_VERSION=`echo $VERSION|cut -d. -f1-3`.`date +%Y%m%d%H%M%S`
+szReplaceFile=`echo $szSourceBase | cut -d- -f1`_$LAUNCHPAD_VERSION
 
 if [ "$VERSION" == "`cat LAST_VERSION.$szPlatform`" ]; then
 	echo "version $VERSION already built, exiting"
@@ -22,10 +30,11 @@ fi
 tar xfz $szSourceFile
 mv $szSourceFile $szReplaceFile.orig.tar.gz
 
-cd $szSourceBase
+mv $szSourceBase $LAUNCHPAD_VERSION
+cd $LAUNCHPAD_VERSION
 cp -r ../$szPlatform/$szBranch/debian .
 # create dummy changelog entry
-echo "rsyslog ($VERSION-0adiscon1$szPlatform) $szPlatform; urgency=low" > debian/changelog
+echo "rsyslog ($LAUNCHPAD_VERSION-0adiscon1$szPlatform) $szPlatform; urgency=low" > debian/changelog
 echo "" >> debian/changelog
 echo "  * daily build" >> debian/changelog
 echo "" >> debian/changelog
@@ -44,5 +53,5 @@ dput -f ppa:adiscon/$szBranch `ls *.changes`
 #cleanup
 echo $VERSION >LAST_VERSION.$szPlatform
 #exit # do this for testing
-rm -rf $szSourceBase
+rm -rf $LAUNCHPAD_VERSION
 rm -v $szReplaceFile*.dsc $szReplaceFile*.build $szReplaceFile*.changes $szReplaceFile*.upload *.tar.gz
