b2ab37654f64c99f2fcc6b408064b25a556e031b
Rainer Gerhards <rgerhards@adiscon.com>
2015-01-22 10:29:10 +0100

    ability for automatic daily build for all active projects
    also some cleanup (much needed...)

--- scripts/auto_daily.sh
+++ scripts/auto_daily.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
 echo build all daily packages
 
+#libestr
+cd ~/proj/rsyslog-pkg-ubuntu/libestr
+rm -fv *.tar.gz
+cp -v ~/proj/libestr/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
+cp -v ~/proj/libestr/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel master
+cp -v ~/proj/libestr/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
+
+#liblogging
+cd ~/proj/rsyslog-pkg-ubuntu/liblogging
+rm -fv *.tar.gz
+cp -v ~/proj/liblogging/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
+cp -v ~/proj/liblogging/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel master
+cp -v ~/proj/liblogging/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
+
+#librelp
+cd ~/proj/rsyslog-pkg-ubuntu/librelp
+rm -fv *.tar.gz
+cp -v ~/proj/librelp/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
+cp -v ~/proj/librelp/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel master
+cp -v ~/proj/librelp/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
+
+#liblognorm
+cd ~/proj/rsyslog-pkg-ubuntu/liblognorm
+rm -fv *.tar.gz
+cp -v ~/proj/liblognorm/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
+cp -v ~/proj/liblognorm/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel master
+cp -v ~/proj/liblognorm/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
+
 #rsyslog
 cd ~/proj/rsyslog-pkg-ubuntu/rsyslog
 rm -fv *.tar.gz
 cp -v ~/proj/rsyslog/*.tar.gz .
-~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel v8-devel
 cp -v ~/proj/rsyslog/*.tar.gz .
-~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel v8-devel
--- scripts/auto_daily_project.sh
+++ scripts/auto_daily_project.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-# Build daily snapshot rsyslog package
+# Build daily snapshot package
 # ***current working directory must be project dir (e.g. rsyslog)***
 
 # Note: Launchpad does not work with the git hash inside the version
 # number, because it checks if the version number as whole is larger
 # than what exists. This obviously is not always the case with hashes.
 # As such, we rename the version to todays date and time. It looks like
-# this works sufficiently good, even when the rsyslog code has the
+# this works sufficiently good, even when the source code has the
 # "right" (hash-based) version number.
 
-#set -o xtrace
+# set -o xtrace
 
 # params
-szPlatform=$1
-szBranch=$2
+szPlatform=$1  # trusty, utopic, ...
+UPLOAD_PPA=$2  # path of the ppa (e.g. v8-devel)
+szBranch=$3    # branch to use (e.g. master)
+	       # Note: this must match the tarball branch
 
 # only a single .tar.gz must exist at any time
 szSourceFile=`ls *.tar.gz`
 szSourceBase=`basename $szSourceFile .tar.gz`
 VERSION=`echo $szSourceBase|cut -d- -f2`
 LAUNCHPAD_VERSION=`echo $VERSION|cut -d. -f1-3`.`date +%Y%m%d%H%M%S`
-szReplaceFile=`echo $szSourceBase | cut -d- -f1`_$LAUNCHPAD_VERSION
+PROJECT=`echo $szSourceBase | cut -d- -f1`
+szReplaceFile="${PROJECT}_$LAUNCHPAD_VERSION"
 
 if [ "$VERSION" == "`cat LAST_VERSION.$szPlatform`" ]; then
 	echo "version $VERSION already built, exiting"
@@ -32,9 +35,12 @@ mv $szSourceFile $szReplaceFile.orig.tar.gz
 
 mv $szSourceBase $LAUNCHPAD_VERSION
 cd $LAUNCHPAD_VERSION
+cp -r ../common/$szBranch/debian .
+# now overwrite with platform-specific stuff (if any)
 cp -r ../$szPlatform/$szBranch/debian .
+
 # create dummy changelog entry
-echo "rsyslog ($LAUNCHPAD_VERSION-0adiscon1$szPlatform) $szPlatform; urgency=low" > debian/changelog
+echo "$PROJECT ($LAUNCHPAD_VERSION-0adiscon1$szPlatform) $szPlatform; urgency=low" > debian/changelog
 echo "" >> debian/changelog
 echo "  * daily build" >> debian/changelog
 echo "" >> debian/changelog
@@ -48,7 +54,7 @@ debuild -S -sa -rfakeroot -k"$PACKAGE_SIGNING_KEY_ID"
 cd ..
 
 # Upload changes to PPA now!
-dput -f ppa:adiscon/$szBranch `ls *.changes`
+dput -f ppa:adiscon/$UPLOAD_PPA `ls *.changes`
 
 #cleanup
 echo $VERSION >LAST_VERSION.$szPlatform
