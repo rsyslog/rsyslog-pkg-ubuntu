39e4ce07b3e79b9d1d27fd07669e64e8a1090dd8
Rainer Gerhards <rgerhards@adiscon.com>
2015-01-22 12:32:38 +0100

    add libgt to daily builds

--- scripts/auto_daily.sh
+++ scripts/auto_daily.sh
@@ -41,10 +41,22 @@ cp -v ~/proj/liblognorm/*.tar.gz .
 cp -v ~/proj/liblognorm/*.tar.gz .
 ~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
 
+#libgt
+cd ~/proj/rsyslog-pkg-ubuntu/libgt
+rm -fv *.tar.gz
+cp -v ~/proj/libgt/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
+cp -v ~/proj/libgt/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel master
+cp -v ~/proj/libgt/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
+
 #rsyslog
 cd ~/proj/rsyslog-pkg-ubuntu/rsyslog
 rm -fv *.tar.gz
 cp -v ~/proj/rsyslog/*.tar.gz .
 ~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel v8-devel
+cp -v ~/proj/rsyslog/*.tar.gz .
+~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh utopic v8-devel v8-devel
 cp -v ~/proj/rsyslog/*.tar.gz .
 ~/proj/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel v8-devel
