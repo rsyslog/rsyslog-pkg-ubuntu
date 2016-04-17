1630addb72819a51d73932245f96669684b3d60b
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-17 10:38:53 +0000

    add support for Ubuntu 16.04LTS

--- scripts/auto_daily.sh
+++ scripts/auto_daily.sh
@@ -8,79 +8,61 @@ date
 
 
 #libfastjson
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libfastjson
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/libfastjson/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/libfastjson/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master
-cp -v $INFRAHOME/repo/libfastjson/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master
-cp -v $INFRAHOME/repo/libfastjson/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master
+for PROJECT in libfastjson libestr liblogging \
+               librelp liblognorm libgt libksi rsyslog
+do
+	for PLATFORM in precise trusty vidid wily xenial
+	do
+		cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/$PROJECT
+		rm -fv *.tar.gz
+		cp -v $INFRAHOME/repo/$PROJECT/*.tar.gz .
+		$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+	done
+done
+exit
 
 
 #libestr
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libestr
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/libestr/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/libestr/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
-cp -v $INFRAHOME/repo/libestr/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master 
-cp -v $INFRAHOME/repo/libestr/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master 
+for PLATFORM in precise trusty vidid wily xenial
+	cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libestr
+	rm -fv *.tar.gz
+	cp -v $INFRAHOME/repo/libestr/*.tar.gz .
+	$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+done
 
 
 #liblogging
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/liblogging
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/liblogging/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/liblogging/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
-cp -v $INFRAHOME/repo/liblogging/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master 
-cp -v $INFRAHOME/repo/liblogging/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master 
+for PLATFORM in precise trusty vidid wily xenial
+	cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/liblogging
+	rm -fv *.tar.gz
+	cp -v $INFRAHOME/repo/liblogging/*.tar.gz .
+	$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+done
 
 
 #librelp
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/librelp
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/librelp/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/librelp/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
-cp -v $INFRAHOME/repo/librelp/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master 
-cp -v $INFRAHOME/repo/librelp/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master 
+for PLATFORM in precise trusty vidid wily xenial
+	cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/librelp
+	rm -fv *.tar.gz
+	cp -v $INFRAHOME/repo/librelp/*.tar.gz .
+	$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+done
 
 #liblognorm
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/liblognorm
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/liblognorm/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/liblognorm/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
-cp -v $INFRAHOME/repo/liblognorm/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master 
-cp -v $INFRAHOME/repo/liblognorm/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master 
+for PLATFORM in precise trusty vidid wily xenial
+	cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/liblognorm
+	rm -fv *.tar.gz
+	cp -v $INFRAHOME/repo/liblognorm/*.tar.gz .
+	$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+done
 
 #libgt
-cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libgt
-rm -fv *.tar.gz
-cp -v $INFRAHOME/repo/libgt/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh precise v8-devel master
-cp -v $INFRAHOME/repo/libgt/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh trusty v8-devel master 
-cp -v $INFRAHOME/repo/libgt/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh vivid v8-devel master 
-cp -v $INFRAHOME/repo/libgt/*.tar.gz .
-$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh wily v8-devel master 
+for PLATFORM in precise trusty vidid wily xenial
+	cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libgt
+	rm -fv *.tar.gz
+	cp -v $INFRAHOME/repo/libgt/*.tar.gz .
+	$INFRAHOME/repo/rsyslog-pkg-ubuntu/scripts/auto_daily_project.sh $PLATFORM v8-devel master
+done
 
 #libksi
 cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/libksi
