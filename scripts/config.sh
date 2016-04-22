64f6cd389a6e81955869938684e03da08035577c
Florian Riedl <friedl@adiscon.com>
2016-04-22 09:02:56 +0000

    Xenial builds

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,6 @@
-PLATFORM="precise trusty vivid wily"
+PLATFORM="precise trusty vivid wily xenial"
 ARCHTECT="amd64 i386"
-BRANCHES="v8-stable"
+BRANCHES="v8-stable v8-stable-testing"
 # GPG KEY
 KEY_ID=AEF0CF8E
 PPA=ppa:adiscon
