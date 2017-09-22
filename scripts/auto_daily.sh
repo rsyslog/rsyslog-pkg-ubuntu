f9e0150b43902db3e60fb9011babaec0eff308d5
Florian Riedl <friedl@adiscon.com>
2017-09-22 13:16:46 +0000

    Several fixes in package files for daily builds

--- scripts/auto_daily.sh
+++ scripts/auto_daily.sh
@@ -13,7 +13,7 @@ set -x
 for PROJECT in libfastjson libestr liblogging \
                librelp liblognorm libgt libksi rsyslog
 do
-	for PLATFORM in precise trusty xenial zesty artful
+	for PLATFORM in precise trusty xenial zesty # artful
 	do
 		cd $INFRAHOME/repo/rsyslog-pkg-ubuntu/$PROJECT
 		rm -fv *.tar.gz
