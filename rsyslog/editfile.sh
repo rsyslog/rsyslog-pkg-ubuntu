a032001c66d3c91b22efca7ab41c2a4fc43a3575
Rainer Gerhards <rgerhards@adiscon.com>
2020-10-20 12:09:40 +0200

    bump definitions for 8.2010 upstream release (in regard to OBS)
    This updates definitions so that SuSe OBS can properly build the
    new release. No guarantee on PPA or others.

--- rsyslog/editfile.sh
+++ rsyslog/editfile.sh
@@ -1,1 +1,6 @@
-vi $(find . -name $1 | grep v8-st|grep -v Deb)
+vi $(find . -name $1 | grep v8-st)
+# use the following if you do NOT want to edit the generic
+# Debian definitions; note: Debian packages for all versions are
+# build with these definitions and the changelog must be updated on
+# a new upstream release
+#vi $(find . -name $1 | grep v8-st|grep -v Deb)
